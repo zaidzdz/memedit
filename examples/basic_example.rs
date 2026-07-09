3629320c0c3f64f7d8d7945813e4932854a572e9
zaidzdz <81050752+zaidzdz@users.noreply.github.com>
2026-07-09 11:41:49 -0400

    added example

---
+++ examples/basic_example.rs
@@ -0,0 +1,16 @@
+use memedit::process::Process;
+use memedit::memory::{read_mem,write_mem};
+fn main() {
+    let health_address: usize =  0x7060E5800;
+
+    let process: Process = Process::open_by_name("Game").unwrap(); // Attach to process with name "Game"
+
+    let mut value: f32 = read_mem::<f32>(&process, health_address).unwrap(); // Read a f32 value (float) at the "health_address"
+    println!("{}", value);
+
+    write_mem::<f32>(&process, health_address,200.0).unwrap(); // Write 200 to the "health_address"
+
+    value = read_mem::<f32>(&process, health_address).unwrap(); // Read the same address again
+    println!("{}", value);
+
+}
